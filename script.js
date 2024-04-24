2369b31c42125662858dac2dbea31a98b18cdd3b
MysticVanguard <johnthomasparis@icloud.com>
2024-04-24 13:35:29 -0400

    Add struggle

--- script.js
+++ script.js
@@ -15,11 +15,23 @@ async function embraceDarkness(){
     const keyhole = "e81ae6b506c82b3981e2d4745660efde01fa62eebc17767c3d60ee8c814880b6";
     key = await generateSHA256(key);
     if (key === keyhole) {
-        console.log("Correct")
+        window.location.href = "../struggle/index.html";
     } else {
-        console.log("Wrong")
+        alert("Embrace the darkness.")
     }
 }
+
+async function acceptance(){
+    var key = document.getElementById("gate").value;
+    const keyhole = "c222f701cff59062af75056ce9fde7c25f6faa82a2fc9fed80f2cf1b73842469";
+    key = await generateSHA256(key);
+    if (key === keyhole) {
+        window.location.href = "../index.html";
+    } else {
+        alert("Prepare.")
+    }
+}
+
 
 async function generateSHA256(str) {
     const encoder = new TextEncoder();

036d51acfbca945e8efb04290a4831fa9e6c88ff
MysticVanguard <johnthomasparis@icloud.com>
2024-05-03 12:33:52 -0400

    Less vague hints, Harder to skip ahead

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
 function unlockGate() {
     var key = document.getElementById("gate").value;
+    var new_key = key.toLowerCase().replaceAll(' ', '_').replaceAll('.', '')
     key = btoa(key);
     var keyhole = "RGVhdGggd2lsbCBjb21lLg==";
-
     if(key === keyhole) {
-        window.location.href = "grim_hand/index.html";
+        window.location.href = "grim_hand/"+new_key+".html";
     } else {
-        alert("You have not accepted.");
+        alert("You have not accepted. Look past reality.");
     }
 }
 
 async function embraceDarkness(){
     var key = document.getElementById("gate").value;
     const keyhole = "e81ae6b506c82b3981e2d4745660efde01fa62eebc17767c3d60ee8c814880b6";
-    key = await generateSHA256(key);
-    if (key === keyhole) {
-        window.location.href = "../struggle/index.html";
+    var changed_key = await generateSHA256(key);
+    key = key.toLowerCase().replaceAll(' ', '_').replaceAll('.', '')
+    if (changed_key === keyhole) {
+        window.location.href = "../struggle/"+key+".html";
     } else {
-        alert("Embrace the darkness.")
+        alert("Four words. Two senses needed.")
     }
 }
 
 async function acceptance(){
     var key = document.getElementById("gate").value;
     const keyhole = "c222f701cff59062af75056ce9fde7c25f6faa82a2fc9fed80f2cf1b73842469";
-    key = await generateSHA256(key);
-    if (key === keyhole) {
-        window.location.href = "../finality/index.html";
+    var changed_key = await generateSHA256(key);
+    key = key.toLowerCase().replaceAll(' ', '_').replaceAll('.', '')
+    if (changed_key === keyhole) {
+        window.location.href = "../finality/"+key+".html";
     } else { 
-        alert("Prepare.")
+        alert("Learn from the past. Embrace their words.")
     }
 }
 
 async function finalKey(){
     var key = document.getElementById("gate").value;
     const keyhole = "013408980555ca2a77fc375be53306a8fe4e801af6bb289bd51d6e80f52e8f65"
-    key = await generateSHA256(key);
-    console.log(key);
-    if (key === keyhole) {
-        window.location.href = "../secret/main/index.html";
+    var changed_key = await generateSHA256(key);
+    key = key.split(' ')[1]
+    if (changed_key === keyhole) {
+        window.location.href = "../secret/main/"+key+".html";
     } else {
-        alert("Go back and find the question first.")
+        alert("Find the question? Answer It Too")
     }
 }
 
